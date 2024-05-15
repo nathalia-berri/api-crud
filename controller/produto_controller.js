3fbc7dfca1c6ee142542757cc723fc232cad0bf4
Nathalia Berri <142815318+nathalia-berri@users.noreply.github.com>
2024-05-14 21:22:18 -0300

    Atualizações aula 14/05/2024
    Inserção do CREATE e do UPDATE, ficando pendente só o DELETE.
    Uso da extensão Rest Client.

--- controller/produto_controller.js
+++ controller/produto_controller.js
@@ -1,5 +1,6 @@
 import connect from "../config/connection.js";
 
+// objeto para troca de dados
 let produto = {};
 const con = await connect(); // conexão assíncrona
 
@@ -13,4 +14,37 @@ produto.all = async function (req, res) // res = resign (entregar) | método do 
     }
 }
 
-export {produto} // exportar o objeto
+produto.create = async function(req, res) {
+    try {
+        let produto = req.body; // vem do cliente
+        let sql = "INSERT INTO produto (id_produto, nome_produto, descricao_produto, preco_produto, id_unid_med) VALUES (?,?,?,?,?)";
+        let values = [produto.id_produto, produto.nome_produto, produto.descricao_produto, produto.preco_produto, produto.id_unid_med];
+        let result = await con.query(sql, values); // mandar executar a inserção
+        res.send({ // objeto
+            status: "Insercao Efetuada com sucess!",
+            result: result
+        })
+    } catch (e) {
+        console.log("Erro..............", e);
+    }
+}
+
+/*
+produto.update = async function(req, res) {
+    try {
+        let nome = req.params.nome_produto;
+        let produto = req.body;
+        let sql = "UPDATE produto SET id_produto=?, nome_produto=?, descricao_produto=?, preco_produto=?, id_unid_med=?";
+        let values = [produto.id_produto, produto.nome_produto, produto.descricao_produto, produto.preco_produto, produto.id_unid_med];
+        let result = await con.query(sql, values);
+        res.send ({
+            status: "Atualizacao do : " + id_produto, nome_produto, descricao_produto, preco_produto, id_unid_med,
+            result: result
+        })
+    } catch (e) {
+        console.log("Erro..............", e);
+    }
+}
+*/
+
+export {produto} // exportar o objeto para o servidor
--- routes/produto_routes.js
+++ routes/produto_routes.js
@@ -2,10 +2,9 @@ import express from "express";
 import {produto} from "../controller/produto_controller.js";
 
 let router = express.Router();
-
 router.get('/produto', produto.all); // get = método que pega os dados
-
+router.post('/produto', produto.create);
+router.put('/produto/:nome_produto', produto.update)
 //definir as demais rotas para o restante do CRUD da API
-// router.post
 
 export {router};
